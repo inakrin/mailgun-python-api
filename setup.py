d9e5270f2b98065d0849e41d7ea6c37eb8920392
Fedor Paligin <f.paligin@gmail.com>
2018-01-23 16:39:51 +0700

    Mailgun update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='mailgunapi-client',
-      version='1.23',
+      version='1.24',
       description='Python Wrapper around MailGun HTTP API Upgraded',
       author='Zach Goldberg',
       author_email='zach@zachgoldberg.com',
