bf7ccfaf07de712bbee9b7ca54c1d91df30126fe
Fedor Paligin <f.paligin@gmail.com>
2020-05-12 03:14:44 +0700

    python3

--- mailgun/__init__.py
+++ mailgun/__init__.py
@@ -1,1 +1,7 @@
-from api import MailgunAPI, MailgunException, RouteExpression
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from future import standard_library
+standard_library.install_aliases()
+from .api import MailgunAPI, MailgunException, RouteExpression
